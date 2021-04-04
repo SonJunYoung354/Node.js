c9203e306a92c6fc895576161cac2ce2d70b3022
SonJunYoung354 <56296503+SonJunYoung354@users.noreply.github.com>
2021-04-04 19:51:14 +0900

    2021-04-04

--- 4장/restServer.js
+++ 4장/restServer.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const users = {};
 
 http.createServer((req,res) =>{
+    //Get 메서드는 페이지 요청 하는것임으로 html 파일 읽어서 전송
     if(req.method === 'GET'){
         if(req.url === '/'){
             return fs.readFile('C:/Users/준영/Desktop/Node.js/4장/restFront.html', (err,data)=>{
@@ -19,9 +20,12 @@ http.createServer((req,res) =>{
                 }
                 res.end(data);
             });
+        //AJAX 요청을 처리하는 /users 에서는 user 데이터 전송
+        // JSON 형식으로 보내기 위해 (JSON.stringify(users) 사용
         }else if(req.url === '/users') {
             return res.end(JSON.stringify(users));
         }
+        // html,css 파일이 있으면 데이터 전송 없으면 404 에러 응답
         return fs.readFile(`.${req.url}`, (err, data) => {
             if (err){
                 res.writeHead(404, 'NOT FOUND');
@@ -29,12 +33,17 @@ http.createServer((req,res) =>{
             }
             return res.end(data);
         });
+        //POST와 PUT 메서드는 클라이언트로부터 데이터를 받음.
+        // req.on('data',(data)=>{
+        // return req.on('end' , () => {
+        // READsTREAM으로 요청과 같이 들어오는 요청 본문을 받을수 있음. 단 문자열임으로 JSON으로 포멧 하는 JSON.parse 필수    
     }else if ( req.method === 'POST'){
         if(req.url === '/users'){
             let body = '';
             req.on('data',(data)=>{
                 body += data;
             });
+           
             return req.on('end',()=>{
                 console.log('POST 본문(Body):',body);
                 const { name } = JSON.parse(body);
@@ -57,9 +66,11 @@ http.createServer((req,res) =>{
                 return res.end(JSON.stringify(users));
             });
         }
+        // DELETE 메서드 요청이 들어오면
+        //키에 해당하는 사용자 제거
     }else if(req.method === 'DELETE'){
         if(req.url.startsWith('/users/')){
-             
+
             const key = req.url.split('/')[2];
             delete users[key];
             return res.end(JSON.stringify(users));
--- 4장/server1-1.js
+++ 4장/server1-1.js
@@ -1,10 +1,11 @@
 const https = require('https');
 const fs = require('fs');
-const { readdirSync } = require('node:fs');
+
 
 // 서버 요청에 따른 콜백함수 생성
 https.createServer({
-    cert : fs.readFileSync('도메인 인증서 경로');
+    // 인증서에서 발급 받은 인증서 , 비밀키 경로
+    cert : fs.readFileSync('도메인 인증서 경로'),
     key : fs.readFileSync('도메인 비밀키 경로'),
     ca: [
         fs.readFileSync('상위 인증서 경로'),
